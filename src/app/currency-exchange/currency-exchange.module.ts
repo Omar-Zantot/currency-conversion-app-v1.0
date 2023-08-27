48449a349374abc531a04cf1a85cd4c5918e46ed
fareeda <faridaallam2002@gmail.com>
2023-08-27 15:40:09 +0300

    comapre data

--- src/app/currency-exchange/currency-exchange.module.ts
+++ src/app/currency-exchange/currency-exchange.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ConverterComponent } from './converter/converter.component';
 import { CompareComponent } from './compare/compare.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CustomSelectComponent } from './custom-select/custom-select.component';
+
 
 @NgModule({
   declarations: [ConverterComponent, CompareComponent, CustomSelectComponent],
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   exports: [ConverterComponent, CompareComponent, CustomSelectComponent],
 })
 export class CurrencyExchangeModule {}
